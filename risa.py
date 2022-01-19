fe5712626084a5017cabf168f2533ff0c3549df3
Valentin <valentinlt4@gmail.com>
2022-01-19 16:10:43 -0300

    features

--- risa.py
+++ risa.py
@@ -1,16 +1,20 @@
-def risa():
-    import random
-    largo = int(input("Largo de la risa:"))
-    for i in range(largo):
-        if i % 2 == 0:
-            n = random.random()
-            if (n <= 0.6):
-                print("J",end = '')
-            elif (n > 0.6 and n <= 0.8):
-                print("K", end = '')
-            elif (n > 0.8):
-                print("JJ", end = '')
-        else:
-                print("A", end = '')
+import random
 
-risa()
+def end(n):
+  if (n <= 0.6):
+    print("J",end = '')
+  elif (n > 0.6 and n <= 0.8):
+    print("K",end = '')
+  elif (n > 0.9):
+    print ("JJ", end = '')
+
+def laugh():
+    length = int(input("Length:"))
+    for i in range(length):
+      if i % 2 == 0:
+        n = random.random()
+        end(n)
+      else:
+        print("A", end = '')
+
+laugh()
