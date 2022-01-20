042d8e9a00765a3137e7a92e928e507b73a6e908
Valentin <valentinlt4@gmail.com>
2022-01-20 19:42:27 -0300

    comentario sobre pasaje de parametros

--- laugh.py
+++ laugh.py
@@ -1,17 +1,22 @@
 import random
 
-def end(n,res):
+'''Pasaje de parametro por referencia(direccion de memoria) vs valor(valor del parametro)
+    cuando se pasa por valor no se modifica la variable del parametro
+    los tipos compuestos (listas, arrays, etc) se pasan por referencia 
+    los tipos primitivos (numeros, caracteres, etc) se pasan por valor'''
+    
+def end(n,tira):
   if (n <= 0.6):
-   str = res + "J"
+   str = tira + "J"
    return str
   elif (n > 0.6 and n <= 0.8):
-    str = res + "K"
+    str = tira + "K"
     return str
   elif (n > 0.9):
-    str = res + "S"
+    str = tira + "S"
     return str
   else:
-    str = res + "A"
+    str = tira + "A"
     return str
 
 def laugh():
